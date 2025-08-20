9b5865cd444b054857e7764ecafabbcd60706553
Giqus67 <danylenkodenys67@gmail.com>
2025-08-20 12:54:09 +0300

    added print fuction to print messages of sum to Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main{
-    public static void main(String[] args)
+    public static void main(String[] args){
         System.out.println("This application will generate random integer number and sum them!");
         random();
     }
-    
     public static void random(){
        int first = calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
        int second = calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
-       System.out.println("first sum: " + first + "\n" + "second sum: " + second);
+       int third = calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
+       printSums(first, second, third);
     }
 
     public static int calcSum(int ...arg){
@@ -17,5 +17,12 @@ class Main{
             sum += i;
         }
         return sum;
+    }
+
+    public static void printSums(int ...sums){
+        int[] array = sums;
+        for(int i = 0; i < array.length; i++){
+            System.out.println(i + 1 + " sum: " + array[i]);
+        }
     }
 }
