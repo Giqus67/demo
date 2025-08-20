80772a8909a36e68da04b9a3a1150b985b9713c0
Giqus67 <danylenkodenys67@gmail.com>
2025-08-20 11:58:54 +0300

    added function to sum integer digits

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main{
     public static void main(String[] args){
         System.out.println("hello world!");
+        calcSum(1, 2, 3, 4, 5);
+        calcSum(5, 12, 7, 17, 5);
+    }
+    
+    public static void calcSum(int ...arg){
+        int[] array = arg;
+        int sum = 0;
+        for(int i : array){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
