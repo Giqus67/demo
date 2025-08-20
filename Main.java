135b8dbb238292fcd83e213deecde1301c9946d8
Giqus67 <danylenkodenys67@gmail.com>
2025-08-20 12:26:13 +0300

    added text messages to Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 class Main{
     public static void main(String[] args){
+        System.out.println("This application will generate random integer number and sum them!");
         random();
     }
     
     public static void random(){
-        calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
-        calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
+       int first = calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
+       int second = calcSum( (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100));
+       System.out.println("first sum: " + first + "\n" + "second sum: " + second);
     }
 
-    public static void calcSum(int ...arg){
+    public static int calcSum(int ...arg){
         int[] array = arg;
         int sum = 0;
         for(int i : array){
             sum += i;
         }
-        System.out.println(sum);
+        return sum;
     }
 }
